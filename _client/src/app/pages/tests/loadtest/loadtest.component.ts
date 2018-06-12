d9cf4c63f46b80a35e714b84bdc5191faec05407
Akito Hataguchi <hataguchi@motionpicture.jp>
2018-06-12 19:18:08 +0900

    no message

--- _client/src/app/pages/tests/loadtest/loadtest.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sskts-tests-loadtest',
-  styleUrls: ['./loadtest.component.scss'],
-  templateUrl: './loadtest.component.html',
-})
-export class LoadtestComponent { }
