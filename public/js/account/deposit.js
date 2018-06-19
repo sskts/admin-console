34952ae898bf8228386c6f6a01ed2fb6e311a764
masumi jotaki <jotaki@motionpicture.jp>
2018-06-19 16:55:25 +0900

    SSKTS-963 ポイント付与通信成功処理追加

--- public/js/account/deposit.js
+++ public/js/account/deposit.js
@@ -1,4 +1,5 @@
 $(function () {
+    // 読み込んだときの処理
     $('.submit').on('click', depositProcess);
 });
 
@@ -7,7 +8,9 @@ $(function () {
  * @param {Event} event
  */
 function depositProcess(event) {
+    // 押したときにデフォルトで実行される処理のキャンセル
     event.preventDefault();
+    // 通信の設定
     var options = {
         dataType: 'json',
         url: '/account/deposit',
@@ -16,7 +19,7 @@ function depositProcess(event) {
         data: {
             recipient: {
                 id: $('input[name=recipientId]').val(),
-                name: $('input[name=recipientName]').val(),
+                name: $('input[name=recipientFamilyName]').val() + ' ' + $('input[name=recipientGivenName]').val(),
                 url: $('input[name=recipientUrl]').val()
             },
             toAccountNumber: $('input[name=toAccountNumber]').val(),
@@ -25,9 +28,15 @@ function depositProcess(event) {
         }
     };
     var done = function (data, textStatus, jqXhr) {
-        console.log(data);
+        // 通信成功の処理
+        console.log('通信成功');
+        // ポイント付与完了 モーダルを出す
+        $('#grantingPointsDone').modal('toggle');
+        $('.form-control').val('');
     };
     var fail = function (jqXhr, textStatus, errorThrown) {
+        // 通信失敗の処理
+        console.log('通信失敗');
         console.log(jqXhr.responseJSON);
     }
     $.ajax(options).done(done).fail(fail);
